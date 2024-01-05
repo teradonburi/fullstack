95aab890fd7358b5e2f350ecab6452c15feaf926
teradonburi <daikiterai@gmail.com>
2024-01-05 17:38:08 +0900

    logic

--- api/src/app/app.service.ts
+++ api/src/app/app.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import * as math from 'mathjs';
+import * as jsonLogic from 'json-logic-js';
 
 @Injectable()
 export class AppService {
   getHello() {
-    const result = math.evaluate('sin(45 deg) ^ 2');
-    return { message: 'Hello World!', result };
+    const mathResult = math.evaluate('sin(45 deg) ^ 2');
+    const logicResult = jsonLogic.apply({
+      and: [{ '>': [3, 1] }, { '<': [1, 3] }],
+    });
+    return { message: 'Hello World!', mathResult, logicResult };
   }
 }
