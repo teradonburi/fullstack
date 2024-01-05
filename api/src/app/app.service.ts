58a0cf376310b0d165506f4afe94d675e3b10882
teradonburi <daikiterai@gmail.com>
2024-01-05 16:25:46 +0900

    mathjs

--- api/src/app/app.service.ts
+++ api/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import * as math from 'mathjs';
 
 @Injectable()
 export class AppService {
   getHello() {
-    return { message: 'Hello World!' };
+    const result = math.evaluate('sin(45 deg) ^ 2');
+    return { message: 'Hello World!', result };
   }
 }
